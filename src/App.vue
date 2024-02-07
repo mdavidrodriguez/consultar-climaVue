f8270dcb4740861e880815c590d609e55f7386a9
mdavidrodriguez <mdavidrodriguez@unicesar.edu.co>
2024-02-07 15:40:58 -0500

    Terminando app consultar clima

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Formulario from './components/Formulario.vue';
+import useClima from './composables/useClima';
+import Clima from './components/Clima.vue';
+import Spinner from './components/Spinner.vue';
+import Alerta from './components/Alerta.vue';
+const { obtenerClima, clima, mostrarClima, cargando, error } = useClima()
+</script>
+
+<template>
+  <h1 class="titulo">Buscador de clima</h1>
+  <div class="contenedor buscador-clima">
+    <Formulario @obtener-clima="obtenerClima" />
+    <Alerta v-if="error">{{ error }}</Alerta>
+    <Spinner v-if="cargando" />
+    <Clima v-if="mostrarClima" :clima="clima" />
+  </div>
+</template>
+
